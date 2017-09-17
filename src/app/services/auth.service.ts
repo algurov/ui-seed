027c7dcc54f35bd5c98f1fe5f304d2d052f310fd
algurov <al_gurov@mail.ru>
2017-09-18 04:06:51 +0700

    auth test

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,7 +6,7 @@ import { API_BASE_URL } from '../services/constants';
 import { RequestBase } from '../services/request.base';
 import { Router } from '@angular/router';
 import { OAuthService } from 'angular2-oauth2/oauth-service';
-
+import { AUTH_SERVER_URL, AUTH_CONSUMER_URL } from './constants';
 @Injectable()
 export class AuthService extends RequestBase {
   constructor(public http: Http, public router : Router, public oauthService : OAuthService) {
@@ -20,8 +20,20 @@ export class AuthService extends RequestBase {
 
 url: string ='http://82.202.236.172:9999/auth-server/oauth/authorize?response_type=code&client_id=client_seed&state=client_id%3Dclient_seed%26goto%3Dhttp%253A%252F%252F82.202.236.172%253A3000%252Fmain%26gotoOnFail%3Dhttp%253A%252F%252F82.202.236.172%253A3000%252Flogin&redirect_uri=http://82.202.236.172:9998/oauth2-consumer/authorize';
 
+  prepareAuthLink(): string {
+    let result = AUTH_SERVER_URL + '?response_type=code&client_id=client_seed&state=';
+    let goto = encodeURIComponent(API_BASE_URL + '/main');
+    console.log('goto    ' + goto);
+    let gotoOnFail = encodeURIComponent(API_BASE_URL + '/login');
+    console.log('gotoOnFail    ' + gotoOnFail);
+    let state = encodeURIComponent('client_id=client_seed&goto=' + goto + '&gotoOnFail=' + gotoOnFail);
+    console.log('state    ' + state);
+    result = result + state + '&redirect_uri=' + AUTH_CONSUMER_URL;
+    console.log(result);
+    return result;
+  }
   login(userLogin: string, userPassword: string): void {
-      this.oauthService.loginUrl = this.url;
+      this.oauthService.loginUrl = this.prepareAuthLink();
       this.oauthService.initImplicitFlow();
   }
 
--- src/app/services/constants.ts
+++ src/app/services/constants.ts
@@ -1,2 +1,6 @@
 export const MOBILE = (typeof window !== 'undefined') ? (window.screen.availWidth < 800) : true;
 export const API_BASE_URL: string = `http://${HOST}:${PORT}`;
+export const AUTH_SERVER_BASE_URL = `http://${HOST}:9999`;
+export const AUTH_CONSUMER_BASE_URL = `http://${HOST}:9998`;
+export const AUTH_SERVER_URL: string = AUTH_SERVER_BASE_URL + '/auth-server/oauth/authorize';
+export const AUTH_CONSUMER_URL: string = AUTH_CONSUMER_BASE_URL + '/oauth2-consumer/authorize';
