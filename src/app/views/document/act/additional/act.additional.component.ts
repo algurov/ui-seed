bcc96a256c56de0963246111f261a82c214e21ff
algurov <al_gurov@mail.ru>
2018-07-26 07:22:09 +0700

    Initial

--- src/app/views/document/act/additional/act.additional.component.ts
+++ src/app/views/document/act/additional/act.additional.component.ts
@@ -1,10 +1,19 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {DocumentService} from "../../../../services/document.service";
 
 @Component({
   selector: 'act-additional',
   templateUrl: './act.additional.component.html',
   styleUrls: ['./act.additional.component.scss']
 })
-export class ActAdditionalComponent {
+export class ActAdditionalComponent implements OnInit{
   @Input() data: any;
+
+  constructor(private documnetService: DocumentService) {}
+  ngOnInit() {
+      this.documnetService.getApplicationList(0,1).subscribe(res => {
+          console.log(res);
+      })
+  }
+
 }
